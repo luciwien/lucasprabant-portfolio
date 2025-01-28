5d5305bad137d3af2d17074536a3161deb208950
Lucas Prabant <lprabant@gmail.com>
2025-01-28 10:38:57 +0100

    removed templateing

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -60,9 +60,6 @@ export default async function IndexRoute({
         <Suspense>
           <Footer />
         </Suspense>
-        <Suspense>
-          <IntroTemplate />
-        </Suspense>
       </div>
       {(await draftMode()).isEnabled && <LiveVisualEditing />}
     </>
